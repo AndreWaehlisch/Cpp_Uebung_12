a348196c769451275fe313db0df3be548fe11f5f
AndreWaehlisch <andre.waehlisch@physik.tu-berlin.de>
2015-01-25 23:33:44 +0100

    aufg2
    Signed-off-by: AndreWaehlisch <andre.waehlisch@physik.tu-berlin.de>

--- aufg2.cpp
+++ aufg2.cpp
@@ -1,8 +1,21 @@
 #include <iostream>
+#include <fstream>
+#include "helfer.h"
 
 using namespace std;
 
 int main()
 {
+	const int N = 10;
+	ifstream eingabe("datei.txt");
+	VecDoub a(N), b(N), c(N);
+	int i = 0;
 
+	while ( eingabe.good() && !eingabe.eof() )
+		eingabe >> a[i++] >> b[i] >> c[i];
+
+	for (int k=0; k<N; k++)
+		cout << a[k] << ' ' << b[k] << ' ' << c[k] << endl;
+
+	eingabe.close();
 }
